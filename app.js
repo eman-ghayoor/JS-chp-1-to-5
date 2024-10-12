6da918ba531aa685194be09d462cb4fcc517a5ea
eman-ghayoor <emanghayoor1@gmail.com>
2024-10-12 11:31:39 -0700

    JS chp 1 to 5

---
+++ app.js
@@ -0,0 +1,276 @@
+//                  //////////// chp 1.........////////
+// Q no 1...//
+alert("alert box");
+
+// Q no 2...//
+alert ("Error! please enter a valid password");
+
+// Q no 3...//
+alert ("Welcome to JS Land\n Happy Coding! ");
+
+// Q no 4...// 
+alert ("Welcome to JS Land");
+alert ("hello coding \nprevent this page from creating additional dialogue");
+
+// Q no 5..//
+alert ("hello... I can run JS through my web browser's console");
+
+//                           ///////// chp 2.......//////////
+// // Q no 1...//
+let username = "eman";
+console.log (username);
+
+// // Q no 2..//
+let myName = "Eiman Ghayour";
+console.log (`hey my name is ${myName}`);
+
+// // Q no 3..//
+let message = "Hello World";
+alert (message);
+
+// // Q no 4...//
+alert (myName);
+let Age = "20 years";
+alert (Age);
+let work = "Mobile Application Development";
+alert (work);
+
+// // Q no 5...//
+let food = "PIZZA\nPIZZ\nPIZ\nPI\nPI";
+alert (food);
+
+// // Q no 6...//
+let email = "my email address @gmail.com";
+alert (email);
+
+// // Q no 7...//
+let book = "i am trying to learn from the Book A Samrter Way To Learn Javascript";
+alert(book);
+
+// Qno 8...//
+document.write("<h2> Yah! i can write html content through javascript </h2>");
+
+// // Q no 9...//
+let img = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert (img);
+
+                    ////////////// chp no 3.........////////
+// // chp no 1...//
+let myAge = "I am 15 years old"
+alert (myAge);
+
+// Q no 2...//
+let visit = "you have visited this site 14 times";
+alert (visit);
+document.write ("<h2> Q no 3 </h2>");
+
+// Q no 3...//
+let birthYear = "1990";
+document.write(`Your birth year is ${birthYear} <br> Data type of my declared variable is number `)
+document.write("<h2> Q no 4 </h2>");
+
+// Q no 4...//
+let productTitle = "T-shirt";
+let quantity = 5;
+let customerName = "John Doe";
+document.write  (`${customerName} ordered ${quantity} ${productTitle}(s) on XYZ Clothing store.`)
+
+             ////////// chp no 4.........///////////
+// Q no 1...//
+let a = 1;
+let b = 2;
+let c = 3;
+console.log (a+b+c);
+
+// Q no 2...//
+document.write ("<h4> Legal Variable Names </h4>");
+document.write ("myVariable <br>");
+document.write ("user_age <br>");
+document.write ("score1 <br>");
+document.write ("isActive <br>");
+document.write ("totalAmount");
+
+document.write ("<h4> Illegal Variable Names </h4>");
+document.write ("1stPlace (cannot start with a number) <br>");
+document.write ("user-name (hyphen is not allowed) <br>");
+document.write ("total amount (spaces are not allowed) <br>");
+document.write ("class (reserved keyword in many languages) <br>");
+document.write ("@username (special characters like @ are not allowed)");
+
+// Q no 3...//
+document.write ("<h4> Rules for Naming JS Variables </h4>");
+document.write ("Variable names can only contain letters, numbers,underscores, and dollar signs. For example, $my_1stVariable <br>");
+document.write ("Variables must begin with a letter, underscore, or dollar sign. For example, $name, _name, or name <br>");
+document.write ("Variable names are case sensitive <br>");
+document.write ("Variable names should not be JS keywords <br>");
+
+                  ////////////// chp no 5...........///////////
+document.write ("<h3> chp no 5 </h3>");
+document.write ("<h4> Q no 1 </h4>");
+// Q no 1...//
+let num1 = 3;
+let num2 = 5;
+console.log (num1+num2);
+document.write (" sum of 3 and 5 is 8");
+
+document.write ("<h4> Q no 2 </h4>");
+// Q no 2...//
+document.write ("<h4> subtraction </h4>");
+let num7 = 3;
+let num8 = 5;
+console.log(num7-num8);
+document.write("subtraction of 3 and 5 is -2");
+document.write ("<h4> multiplication </h4>");
+let num3 = 3;
+let num4 = 5;
+console.log (num3*num4);
+document.write ("multiplication of 3 and 5 is 15");
+document.write ("<h4> division </h4>");
+let num5 = 3;
+let num6 = 5;
+console.log(num5 / num6);
+document.write ("divison of 3 and 5 is 0.6");
+document.write ("<h4> Q modulus </h4>");
+let num9 = 3;
+let num10 = 5;
+console.log(num9 % num10);
+document.write (" modulus of 3 and 5 is 3 ");
+
+document.write ("<h4> Q no 3 </h4>");
+// Q no 3...//
+let decNum = 3;
+document.write ("<h4> ans (a,b) </h4>");
+document.write ("Value after variable declaration is 3");
+// let num = 4;
+document.write ("<h4> ans (c,d) </h4>");
+document.write ("Initial Value : 4");
+let num = 5;
+let Newnum = num++;
+console.log (num);
+document.write ("<h4> ans (e,f) </h4>");
+document.write ("Value after increment is : 6");
+document.write ("<h4> ans (g,h) </h4>");
+document.write ("Value after addition is : 13");
+let Num2 = 12;
+let  = Num2++;
+console.log (Num2);
+document.write ("<h4> ans (i,j) </h4>");
+document.write ("Value after decrement is : 12");
+let Newnum3 = 13;
+let = Newnum3--;
+console.log (Newnum3);
+document.write ("<h4> ans (k,l) </h4>");
+document.write ("The Remainder is : 0");
+let Newnum4 = Newnum3 % 13;
+console.log (Newnum4); 
+
+// Q no 4...//
+document.write ("<h4> Q no 4 </h4>");
+document.write ("Total cost to buy 5 tickets to a movie is 3000PKR");
+let oneticket = 600;
+let totalprice = 600 * 5;
+console .log (totalprice);
+
+// Q no 5...//
+document.write ("<h4> Q no 5 </h4>");
+document.write ("<h2>Table of 4 </h2>");
+document.write ("4x1=4 <br> 4x2=8 <br> 4x3=12 <br> 4x4=16 <br> 4x5=20 <br> 4x6=24 <br> 4x7=28 <br> 4x8=32 <br> 4x9=36 <br> 4x10=40");
+
+// Q no 6...//
+document.write ("<h4> Q no 6 </h4>");
+document.write ("25C is 77F <br>");
+document.write ("70F is 21.11111111111111C <br>");
+let C = 25;
+let F = (C*9/5) + 32;
+console.log (F);
+let Fah = 70;
+let cel = (Fah-32)* 5/9;
+console.log(cel);
+
+// Q no 7...//
+document.write ("<h4> Q no 7 </h4>");
+document.write ("<h2> Shopping Cart </h2>");
+document.write ("Price of item 1 is 650 <br>");
+document.write ("Qunatity of item 1 is 3 <br>");
+document.write ("Price of item 2 is 100 <br>");
+document.write ("Quantity of item 2 is 7 <br>");
+document.write ("Shipping Charges 100 <br>");
+document.write ("Total cost of your order is 2750");
+let itemprice1 = 650;
+let quantityitem1 = 3;
+let itemprice2 = 100;
+let quantityitem2 = 7;
+let shipping = 100;
+let totalcost = itemprice1*quantityitem1 + itemprice2*quantityitem2 + shipping;
+console.log(totalcost);
+
+// Q no 8...//
+document.write ("<h4> Q no 8 </h4>");
+document.write ("<h2> Marksheet </h2>");
+document.write ("Total mark: 980 <br>");
+document.write ("Marked obtained : 840 <br>");
+document.write ("Percentage :82.0408163265306% ");
+let mark = 980;
+let obtmark = 804;
+let Percentage = obtmark / mark * 100
+console.log(Percentage);
+
+// Q no 9...//
+document.write ("<h4> Q no 9 </h4>");
+document.write ("<h2> Currency in PKR </h2>");
+document.write ("Total Currency in PKR is 1748 ");
+let dollar = 104.80;
+let riyal = 28;
+let rupees = dollar*10 + riyal*25 ;
+console.log(rupees);
+
+// Q no 10...//
+document.write ("<h4> Q no 10 </h4>");
+let add = 5;
+let multiply = 10;
+let divide = add / multiply;
+console.log(divide);
+document.write ("ans is 0.5 ");
+
+// Q no 11...//
+document.write ("<h4> Q no 11 </h4>");
+document.write ("<h2> Age Calculator </h2>");
+document.write ("Current Year : 2016 <br>");
+document.write ("Birth Year 1992 <br> ");
+document.write ("your age is 24 ");
+let current = 2016;
+let birth = 1992;
+let yourAge = current-birth;
+console.log(yourAge);
+
+// Q no 12...//
+document.write ("<h4> Q no 12 </h4>");
+document.write ("<h2> The Geometrizer </h2>");
+document.write ("Radius of acircle : 20 <br>");
+document.write ("The circumferences is : 125.67999999999999 <br> ");
+document.write ("The area is 1256.8 ");
+let radius = 20;
+let pii = 3.142;
+console.log(pii*radius);
+let circum = 125.67999999999999 ;
+console.log(pii*radius*radius);
+
+// Q no 13...//
+document.write ("<h4> Q no 13 </h4>");
+document.write ("<h2> The Lifetime Supply Calculator </h2>");
+document.write ("Favourite snack: chocolate chip <br>");
+document.write ("Current age : 15 <br> ");
+document.write ("Estimates maximum Age : 65 <br> ");
+document.write ("Amount of snack per day:3 <br> ");
+document.write ("You will need 150 chocolate chip to last you until the rip old ageof 65 ");
+let Currage = 15;
+let maxiage = 65;
+let day = 3;
+let total = (maxiage-Currage) * day;
+console.log (total);
+
+
+
+
+
+ 
